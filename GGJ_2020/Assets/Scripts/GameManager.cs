ac5a2a2bc3f1c166ba9f1c59ca3a86138575014f
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2020-02-01 13:16:05 +0100

    Add some assets and Game Manager

---
+++ GGJ_2020/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+    public static GameManager instance;
+    public int score;
+    public int player;
+    private float timer;
+    public List<int> objectList;
+
+    private void Awake()
+    {
+        makeSingleton();
+        timer = 10.0f;
+    }
+
+    private void Update()
+    {
+        updateTimer();
+        catchEntry();
+    }
+
+    private void makeSingleton()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void updateTimer()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+            print("HEY");
+    }
+
+    private void catchEntry()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        }
+    }
+}
