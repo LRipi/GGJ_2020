44337fd00fb45dfa3b43dba289462ffa01221f3a
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2020-02-01 08:08:30 +0100

    Creating map  creation pragmatically

---
+++ GGJ_2020/Assets/Scripts/MapCreation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class MapCreation : MonoBehaviour
+{
+    private int width;
+    private int height;
+
+    public Tile leftTile;
+    public Tile rightTile;
+    public Tile bottomTile;
+    public Tile topTile;
+    public Tile rightBottomCornerTile;
+    public Tile rightTopCornerTile;
+    public Tile leftBottomCornerTile;
+    public Tile leftTopCornerTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
