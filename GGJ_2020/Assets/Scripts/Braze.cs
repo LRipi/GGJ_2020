234aa9a6ac20e06e69fc5e08585912d6f6e291f6
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2020-02-01 19:21:58 +0100

    Title screen

--- GGJ_2020/Assets/Scripts/Braze.cs
+++ GGJ_2020/Assets/Scripts/Braze.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Braze : MonoBehaviour
+public class Braze : Workbenches
 {
     // Start is called before the first frame update
     void Start()
--- GGJ_2020/Assets/Scripts/Workbenches.cs
+++ GGJ_2020/Assets/Scripts/Workbenches.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface Workbenches : MonoBehaviour
+public class Workbenches : MonoBehaviour
 {
     bool occupied;
     int processTime;
